0e6428014010b36f1e747eaa3a6e2207e1ce4664
lost <xing8168@sina.cn>
2019-05-31 16:44:44 +0800

    wrote main one time

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
-module.exports = function main(inputs) {
+module.exports = function main(inputs, waitTime) {
     // write your code here...
-    return "price";
+    if(inputs <= 2)
+	return 2 + waitTime * 0.25;
+    else if(inputs > 2 && inputs < 8)
+	return 6 + (inputs -2) * 0.8 + waitTime * 0.25;
+    else
+	return 6 + (inputs - 2) *0.8 + (inputs - 8) * 1.2 + waitTime * 0.25;
+
 };
